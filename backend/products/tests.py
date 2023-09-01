a4d5bc1d11e4c85b859e38a5719c9407f8caecb0
Luke Jefferies <lukejefferiesuk@gmail.com>
2023-09-01 22:15:27 +0100

    add test template for models

--- backend/products/tests.py
+++ backend/products/tests.py
@@ -1,3 +1,80 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestDataIntegrity(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+    
+    def test_fields_not_null(self):
+        """ Test that required field (EAN, Retailer Name, Product Title) cannot be NULL """
+        pass
+
+    def test_verify_unique_constraint(self):
+        """ Verify unique fields (EAN) are enforced """
+        pass
+
+    def test_foregin_key(self):
+        """ Check foregin key constraints (Product, Retailer) are enforced """
+        pass
+
+    def test_boolean_fields(self):
+        """ Ensure boolean fields (On Promotion) only have True/False values """
+        pass
+
+    def test_image_urls(self):
+        """ Check that image URLs are valid and accessible """
+        pass
+
+
+class TestRelationships(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+    
+    def test_products_associated_retailer(self):
+        """ Verify that products are associated with the correct retailers in 'ProductRetailer' model """
+        pass
+
+    def test_product_references(self):
+        """ Ensure each product has valid reference (category, manufacturer, brand) """
+        pass
+
+    def test_price_references(self):
+        """ Validate each price record references an existing product and retailer """
+        pass
+
+
+class TestFunctionality(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+    
+    def test_crud_operations(self):
+        """ Test CRUD operations for each model to ensure we can manipulate records as expected """
+        pass
+
+    def test_promoted_price(self):
+        """ Test promotional price is being applied if on_promotion is True """
+        pass
+
+    def test_querying(self):
+        """ Test querying for product prices by date and retailer to ensure accurate pricing info """
+        pass
+
+    def test_promotion_applied(self):
+        """ Validate promotions can be associated with products and description is stored correctly """
+        pass
+
+
+class TestPerformance(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+    
+    def test_query_performance(self):
+        """ Measure performance of common queries and their response times """
+        pass
+
+    def test_load(self):
+        """ Stress test database with large volume of data to evaluate scalibility """
+        pass
+
+
+# Other TestCases: (Concurrency, Error Handling, Security, Backup and Recovery, Indexing, Cross-Browser/Platform)
